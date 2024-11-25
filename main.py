432af2bdab4f22dee545d03bccc10cec8ce61cba
Saaji <saajid.aliyar@outlook.com>
2024-11-24 20:15:37 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Project will allow you to put your steam link, would then compare your profile with another profile using cssStats.gg
+
+#https://csstats.gg/
+
+#user would have to put their user profile as a "sign in"
+
+#thgen submit new profile to cehck
+
+
+userProfile = input("Enter User Steam Account")
+print("User is " + userProfile )
